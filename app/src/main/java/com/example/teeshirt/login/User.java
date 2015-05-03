b7577fa61c8695c8c31beb3a1a1c4f4051a8e0cf
owuor91 <owuor91@gmail.com>
2015-05-03 13:14:27 +0300

    On item select toast added

--- app/src/main/java/com/example/teeshirt/login/User.java
+++ app/src/main/java/com/example/teeshirt/login/User.java
@@ -4,25 +4,37 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class User extends ActionBarActivity {
     private static ListView listViewLV;
-    private static String[] NAMES = new String[] {"Tom", "Dick", "Larry", "Page", "Kendele"};
+    private static String[] NAMES = new String[] {"Tom", "Dick", "Larry", "Page", "Kendele","Amos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-        listView();
+        showList();
     }
 
-    public void listView(){
+    public void showList(){
         listViewLV = (ListView)findViewById(R.id.listViewLV);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.namelist, NAMES);
         listViewLV.setAdapter(adapter);
+        listViewLV.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String value = (String)listViewLV.getItemAtPosition(position);
+                        Toast.makeText(User.this, value, Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
 
     }
 
